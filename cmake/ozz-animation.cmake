0c10cbf1a2513983ae2c0c308faa42973e561ca3
guo122 <balanuard@163.com>
2019-10-18 11:26:15 +0800

    [build] fix ExternalProjectAdd

--- cmake/ozz-animation.cmake
+++ cmake/ozz-animation.cmake
@@ -1,9 +1,14 @@
 include(ExternalProject)
 
 ExternalProject_Add(ozz-animation
-	GIT_SUBMODULES		3rd/ozz-animation
+	#EP_PREFIX			${CMAKE_ROOT_DIR}/bin-ozz
+	#GIT_SUBMODULES		3rd/ozz-animation
+	#PREFIX				${CMAKE_ROOT_DIR}/Solution
 	SOURCE_DIR			${CMAKE_ROOT_DIR}/3rd/ozz-animation
-	INSTALL_COMMAND		""
+	INSTALL_COMMAND 	cmake . -DCMAKE_INSTALL_PREFIX=${CMAKE_ROOT_DIR}/3rdlib && cmake --build . --config Debug --target install
+	#INSTALL_COMMAND 	"make DESTDIR='${CMAKE_ROOT_DIR}/bin-ozz' install"
+	#INSTALL_COMMAND 	"set(CMAKE_INSTALL_PREFIX ${CMAKE_ROOT_DIR}/bin-ozz)"
+	#INSTALL_COMMAND		"install TARGETS ozz-animation CONFIGURATIONS Debug RUNTIME DESTINATION  ${CMAKE_ROOT_DIR}/bin-ozz"
 	#INSTALL_COMMAND		"RUNTIME DESTINATION bin LIBRARY DESTINATION ${CMAKE_ROOT_DIR}/lib"
 )
 
--- cmake/sfml.cmake
+++ cmake/sfml.cmake
@@ -1,7 +1,8 @@
 include(ExternalProject)
 
 ExternalProject_Add(SFML
-	GIT_SUBMODULES		3rd/SFML
+	#GIT_SUBMODULES		3rd/SFML
 	SOURCE_DIR			${CMAKE_ROOT_DIR}/3rd/SFML
-	INSTALL_COMMAND		""
+	INSTALL_COMMAND     cmake . -DCMAKE_INSTALL_PREFIX=${CMAKE_ROOT_DIR}/3rdlib && cmake --build . --config Debug --target install
+	#INSTALL_COMMAND		""
 )
